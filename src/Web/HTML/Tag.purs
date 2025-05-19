518f100f168e24a71046ea65f3a294e3a3f2d992
UnrelatedString <unrelatedstring@gmail.com>
2025-05-19 16:18:01 -0400

    wait yeah in what world would that actually be a dependency here ???

--- src/Web/HTML/Tag.purs
+++ src/Web/HTML/Tag.purs
@@ -12,3 +12,9 @@ import Web.HTML.HTMLHtmlElement (HTMLHtmlElement)
 foreign import data HTMLTag :: Symbol -> Type
 
 -- and then have class stuff for the interfaces from that lmaoooooooooo
+
+class TagImplements :: Symbol -> Type -> Constraint
+class TagImplements symbol interface
+
+-- and NO FUNDEP because like hey something totally could implement multiple interfaces
+-- plus users can like. define new foreign interfaces for shared functionality etc.
