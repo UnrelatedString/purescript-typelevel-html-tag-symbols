509f076252ef16b42d83d2c41379c93bda482c83
UnrelatedString <unrelatedstring@gmail.com>
2025-05-19 16:08:46 -0400

    ...at this point this has changed so fundamentally that I may as well flatten the branches back down LMAO

--- src/Web/HTML/Tag/TagSymbol.purs
+++ src/Web/HTML/Tag/TagSymbol.purs
@@ -1,11 +1,9 @@
-module Web.HTML.Type.TagSymbol
-  ( class SpecialTagSymbol
-  --, class TagSymbol
-  , SpecialTagSymbols
-  --, TagSymbols
+module Web.HTML.Tag
+  ( HTMLTag
+  , class TagImplements
   ) where
 
-import Prim.Row as Row
+--import Prim.Row as Row
 import Web.HTML
 import Web.HTML.HTMLHtmlElement (HTMLHtmlElement)
 
@@ -13,87 +11,4 @@ import Web.HTML.HTMLHtmlElement (HTMLHtmlElement)
 
 foreign import data HTMLTag :: Symbol -> Type
 
--- and then have class stuff for the interfaces from that and ughhhhh then I need to rename the package to NOT say typelevel and afhadjfh ajkgfherak;g h;erakg herkaghjn;erjkaghnj;keraqhngipuj[erqhngiujperhgnpiejraugnhbpre;jhng;kjrdhgnk;j]
-
--- | A class providing a sealed mapping from tag names to interface types.
-class SpecialTagSymbol :: Symbol -> Type -> Constraint
-class SpecialTagSymbol symbol elem
-  | symbol -> elem
-
-instance Row.Cons symbol elem r SpecialTagSymbols => SpecialTagSymbol symbol elem
-
--- | A mapping from tag names to
--- | the corresponding types which provide bindings to interfaces
--- | beyond the capabilities of a bare `HTMLElement`.
-
-type SpecialTagSymbols :: Row Type
-type SpecialTagSymbols =
-  ( a :: HTMLAnchorElement
-  , area :: HTMLAreaElement 
-  , audio :: HTMLAudioElement 
-  , br :: HTMLBRElement 
-  , base :: HTMLBaseElement 
-  , body :: HTMLBodyElement 
-  , button :: HTMLButtonElement 
-  , canvas :: HTMLCanvasElement 
-  , dl :: HTMLDListElement 
-  , data :: HTMLDataElement 
-  , datalist :: HTMLDataListElement 
-  , div :: HTMLDivElement 
-  , embed :: HTMLEmbedElement 
-  , fieldset :: HTMLFieldSetElement 
-  , form :: HTMLFormElement 
-  , hr :: HTMLHRElement 
-  , head :: HTMLHeadElement 
-  , h1 :: HTMLHeadingElement
-  , h2 :: HTMLHeadingElement
-  , h3 :: HTMLHeadingElement
-  , h4 :: HTMLHeadingElement
-  , h5 :: HTMLHeadingElement
-  , h6 :: HTMLHeadingElement
-  , html :: HTMLHtmlElement 
-  , iframe :: HTMLIFrameElement 
-  , image :: HTMLImageElement 
-  , input :: HTMLInputElement 
-  , keygen :: HTMLKeygenElement -- this isn't on MDN ;_;
-  , li :: HTMLLIElement 
-  , label :: HTMLLabelElement 
-  , legend :: HTMLLegendElement 
-  , link :: HTMLLinkElement 
-  , map :: HTMLMapElement 
-  , media :: HTMLMediaElement 
-  , meta :: HTMLMetaElement 
-  , meter :: HTMLMeterElement 
-  , ins :: HTMLModElement 
-  , del :: HTMLModElement
-  , ol :: HTMLOListElement 
-  , object :: HTMLObjectElement 
-  , optgroup :: HTMLOptGroupElement 
-  , option :: HTMLOptionElement 
-  , output :: HTMLOutputElement 
-  , paragraph :: HTMLParagraphElement 
-  , param :: HTMLParamElement 
-  , pre :: HTMLPreElement 
-  , progress :: HTMLProgressElement 
-  , quote :: HTMLQuoteElement 
-  , script :: HTMLScriptElement 
-  , select :: HTMLSelectElement 
-  , source :: HTMLSourceElement 
-  , span :: HTMLSpanElement 
-  , style :: HTMLStyleElement 
-  , tablecaption :: HTMLTableCaptionElement 
-  , tablecell :: HTMLTableCellElement 
-  , tablecol :: HTMLTableColElement 
-  , tabledatacell :: HTMLTableDataCellElement 
-  , table :: HTMLTableElement 
-  , tableheadercell :: HTMLTableHeaderCellElement 
-  , tablerow :: HTMLTableRowElement 
-  , tablesection :: HTMLTableSectionElement 
-  , template :: HTMLTemplateElement 
-  , textarea :: HTMLTextAreaElement 
-  , time :: HTMLTimeElement 
-  , title :: HTMLTitleElement 
-  , track :: HTMLTrackElement 
-  , ulist :: HTMLUListElement 
-  , video :: HTMLVideoElement 
-  )
+-- and then have class stuff for the interfaces from that lmaoooooooooo
